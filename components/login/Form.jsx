e0f7ef76fec6776338fc2a1d083e58f91d0e7f73
Yasser_saud <ya-1417@outlook.com>
2021-03-31 21:00:27 +0300

    axios

--- components/login/Form.jsx
+++ components/login/Form.jsx
@@ -19,23 +19,10 @@ const Form = () => {
 
   const onSubmit = async ({ email, password }) => {
     try {
-      // const res = await axios.post(
-      //   '/api/user/login',
-      //   { email, password },
-      //   { withCredentials: true }
-      // );
-      const res = await fetch(
-        'https://khadmati-server.herokuapp.com/api/user/login',
-        {
-          method: 'POST',
-          // credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-            Accept: '/',
-          },
-          body: JSON.stringify({ email, password }),
-        }
+      const res = await axios.post(
+        '/api/user/login',
+        { email, password },
+        { withCredentials: true }
       );
       router.push('/');
     } catch (error) {
--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,10 @@ import 'nprogress/nprogress.css';
 import { RecoilRoot, useRecoilState } from 'recoil';
 import { userState } from '../context/recoilStates';
 
-// axios.defaults.baseURL = 'https://khadmati-server.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:5000';
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'https://khadmati-server.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:5000';
+// axios.defaults.baseURL = 'https://angry-albattani-231cdc.netlify.app';
+// axios.defaults.withCredentials = true;
 
 NProgress.configure({
   showSpinner: false,
