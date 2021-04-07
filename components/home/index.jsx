624b70ed3b96e6bb17035bdbe982de0461a8ec2e
Yasser_saud <ya-1417@outlook.com>
2021-04-07 05:35:29 +0300

    svg fix

--- components/home/index.jsx
+++ components/home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const index = ({ user }) => {
   return (
     <div className="container mx-auto">
-      <h1>Home</h1>
+      <h1>Homee</h1>
       {user && <h3>Hello {user.email}</h3>}
     </div>
   );
